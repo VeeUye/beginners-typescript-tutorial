6f230327133f94b6ef6b6a4f5b2f56975fa58912
Vee Uye <vee.uye@gmail.com>
2023-03-23 15:26:37 +0000

    feat: complete 12-record-solution-2

--- src/11-record.problem.ts
+++ src/11-record.problem.ts
@@ -1,7 +1,10 @@
 import { expect, it } from "vitest";
 
 const createCache = () => {
-  const cache: Record<string, string> = {};
+  const cache: {
+    [id: string]: string;
+
+  } = {}
 
   const add = (id: string, value: string) => {
     cache[id] = value;
